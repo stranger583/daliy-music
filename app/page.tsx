70ec790a57ed09cd9106507713874a1431966df1
cozy chen <zed042760@gmail.com>
2023-09-24 23:57:29 +0800

    判斷網址日期不能超過

--- app/page.tsx
+++ app/page.tsx
@@ -15,11 +15,20 @@ interface Props {
 
 async function getData({ searchParams }: Props) {
   const today = new Date();
-  const month = searchParams["month"] ? parseInt(searchParams["month"] as string)?.toString().padStart(2, "0") : (today.getMonth() + 1).toString().padStart(2, "0")
-  const date = (searchParams["date"] as string)?.padStart(2, "0") ?? today.getDate().toString().padStart(2, "0")
-  const year = (searchParams["year"] as string)?.padStart(2, "0") ?? today.getFullYear().toString().padStart(2, "0")
+  const t_month = (today.getMonth() + 1).toString().padStart(2, "0")
+  const t_day = today.getDate().toString().padStart(2, "0")
+  const t_year = today.getFullYear().toString().padStart(2, "0")
 
-  const url = `https://daily-music-api-h0qs.onrender.com/music/get/${year}${month}${date}`;
+  const month = searchParams["month"] ? parseInt(searchParams["month"] as string)?.toString().padStart(2, "0") : t_month
+  const date = (searchParams["date"] as string)?.padStart(2, "0") ?? t_day
+  const year = (searchParams["year"] as string)?.padStart(2, "0") ?? t_year
+  const urlDate = new Date( parseInt(year) , parseInt(month) - 1 ,parseInt(date))
+
+  const isOverDate = urlDate.getTime() > new Date().getTime()
+  const url = isOverDate ? 
+    `https://daily-music-api-h0qs.onrender.com/music/get/${year}${month}${date}`:
+    `https://daily-music-api-h0qs.onrender.com/music/get/${t_year}${t_month}${t_day}`
+    ;
   const response = await fetch(url)
   const data = await response.json()
   return data
